8d2b4506c33e0d6739d5822fce146eee10eda4cc
carolinesamuel <sam.caru@gmail.com>
2017-08-23 10:10:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,40 +17,7 @@ content: `
   </p>`
 };
 
-function createTemplate(data){
-  var title = data.title;
-  var date = data.date;
-  var heading = data.heading;
-  var content = data.content;
-  
-var htmlTemplate=`
-<html>
-    <head>
-        <title> 
-${title}
-             </title>
-        <meta name="viewport" content="width=device-width, initial scale=1">
-          <link href="/ui/style.css" rel="stylesheet" />
-      
-     </head>
-<body>
-    <div class="container">
-    <div>
-        <a href='/'> Home </a>
-    </div>
-    <hr/>
-    ${heading}
-   ${date}
-    <div>
-   ${content}
-    </div>
-    </div>
-</body>        
-</html>
 
-`;
-  return htmlTemplate;
-}
 
 var 'article-two':{
   title: 'Article Two | Mrs. Caroline Samuel',
@@ -63,40 +30,7 @@ content: `
   </p>`
 };
 
-function createTemplate(data){
-  var title = data.title;
-  var date = data.date;
-  var heading = data.heading;
-  var content = data.content;
-  
-var htmlTemplate=`
-<html>
-    <head>
-        <title> 
-${title}
-             </title>
-        <meta name="viewport" content="width=device-width, initial scale=1">
-          <link href="/ui/style.css" rel="stylesheet" />
-      
-     </head>
-<body>
-    <div class="container">
-    <div>
-        <a href='/'> Home </a>
-    </div>
-    <hr/>
-    ${heading}
-   ${date}
-    <div>
-   ${content}
-    </div>
-    </div>
-</body>        
-</html>
 
-`;
-  return htmlTemplate;
-}
 
 var 'article-three':{
   title: 'Article Three | Mrs. Caroline Samuel',
