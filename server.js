8f3cd952b2b6870412a0e4e09fe1da2d7c746788
carolinesamuel <sam.caru@gmail.com>
2017-08-24 20:19:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/main.js', function(req, res){
-    res.sendfile(path.join(__dirname, 'ui', 'main.js'));
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
