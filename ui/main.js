4865a3ff9c87e4293fe3f827f20a59cef1fb0c58
carolinesamuel <sam.caru@gmail.com>
2017-08-25 14:51:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ function moveRight(){
     marginLeft = marginLeft + 10;
     img.stlye.marginLeft = marginLeft + 'px';
 }
-
+element.innerHTMl='roses'
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 //    img.style.marginLeft = '100px'
