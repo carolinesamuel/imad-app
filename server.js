fed8a62899f338eadb428c29863b87e8b993aa3f
carolinesamuel <sam.caru@gmail.com>
2017-08-25 17:37:59 +0530

    [imad-console] Updates server.js

---
+++ server.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+var articles = {
+ 'article-one' : {
+ title: 'Article One | Mrs. Caroline Samuel',
+ heading: 'Article One',
+ date: '23 August 2017',
+content: 
+ `<p>
+ The clever fox jumps over a lazy dog
+ </p>`
+},
+
+ 'article-two':{
+ title: 'Article Two | Mrs. Caroline Samuel',
+ heading: 'Article Two',
+ date: '23 August 2017',
+content: `
+ <p>
+ 
+ Betty bought some butter
+ </p>`
+},
+
+ 'article-three':{
+ title: 'Article Three | Mrs. Caroline Samuel',
+ heading: 'Article Three',
+ date: '23 August 2017',
+content: 
+ ` <p>
+ 
+ She sells sea shells
+ </p>`
+}};
+
+function createTemplate(data){
+ var title = data.title;
+ var date = data.date;
+ var heading = data.heading;
+ var content = data.content;
+ 
+var htmlTemplate=`
+<html>
+    <head>
+        <title> 
+${title}
+             </title>
+        <meta name="viewport" content="width=device-width, initial scale=1">
+          <link href="/ui/style.css" rel="stylesheet" />
+      
+     </head>
+<body>
+    <div class="container">
+    <div>
+        <a href='/'> Home </a>
+    </div>
+    <hr/>
+    ${heading}
+   ${date}
+    <div>
+   ${content}
+    </div>
+    </div>
+</body>        
+</html>
+
+`;
+  return htmlTemplate;
+}
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter=0
+app.get('/counter', function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+app.get('/:articleName', function(req, res) {
+  // articleName = article-one
+  var articleName = req.params.articleName;
+     res.send(createTemplate(articles[articleName]));
+});
+
+//app.get ('/:article-two',function(req,res){
+ // res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+//})
+
+//app.get ('/article-three',function(req,res){
+//  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+//})
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
