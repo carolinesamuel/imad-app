01f886df9b34e372e5c1162a60d89b33119e2fc4
carolinesamuel <sam.caru@gmail.com>
2017-08-25 18:41:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,12 @@ var button=document.getElementById('counter');
 
 var counter = 0;
 
-button.onclick=function(){
+button.onclick = function () {
 
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+
+    
+    
 };
