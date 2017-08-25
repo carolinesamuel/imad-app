b0f207af09dcfed8a80784b8695966aeb22d01cc
carolinesamuel <sam.caru@gmail.com>
2017-08-25 14:25:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ element.innerHTML='New Value';
 //move the image
 var img=document.getElementById('madi');
 
-var marginLeft=0;
+//var marginLeft=0;
 
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.stlye.marginLeft=marginLeft + 'px';
-}
+//function moveRight(){
+ //   marginLeft = marginLeft + 10;
+ //   img.stlye.marginLeft=marginLeft + 'px';
+//}
 
 img.onclick = function () {
     var interval = setInterval(moveRight , 100);
