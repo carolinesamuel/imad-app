dfce4f83645dea516b594548e2713469dadd5d88
carolinesamuel <sam.caru@gmail.com>
2017-08-25 14:16:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 
 function moveRight(){
-    marginLeft=marginLeft + 10;
+    marginLeft = marginLeft + 10;
     img.stlye.marginLeft=marginLeft + 'px';
 }
 
